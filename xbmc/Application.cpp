03cd7f4b67defc5a5fa99b79cdb10c55387c59ab
Stefan Saraev <stefan@saraev.ca>
2016-12-18 11:28:24 -0800

    disable minimize

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -4983,7 +4983,6 @@ bool CApplication::SwitchToFullScreen(bool force /* = false */)
 
 void CApplication::Minimize()
 {
-  g_Windowing.Minimize();
 }
 
 std::string CApplication::GetCurrentPlayer()
