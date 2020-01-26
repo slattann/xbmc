eba5ef9cfc791bdc80fb41077a1b58c38dc6caca
Lukas Rusak <lorusak@gmail.com>
2020-01-26 08:47:30 -0800

    [cmake] CompilerSettings: check the compiler actually supports the c++ standard

--- cmake/scripts/common/CompilerSettings.cmake
+++ cmake/scripts/common/CompilerSettings.cmake
@@ -7,7 +7,22 @@ if(ENABLE_SDBUSCPP)
   set(CMAKE_CXX_STANDARD 17)
 endif()
 
+message("-- Selecting C++${CMAKE_CXX_STANDARD} standard")
 
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 set(CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS} -x assembler-with-cpp")
+
+message("-- Checking to see if CXX compiler supports C++${CMAKE_CXX_STANDARD}")
+
+include(CheckCXXSourceCompiles)
+check_cxx_source_compiles("int main() {
+                            return 0;
+                          }"
+                          HAS_CXX_STANDARD)
+
+if(NOT HAS_CXX_STANDARD)
+  message(FATAL_ERROR "Your C++ compiler does not support C++${CMAKE_CXX_STANDARD}")
+else()
+  message("-- Your C++ compiler supports C++${CMAKE_CXX_STANDARD}")
+endif()
