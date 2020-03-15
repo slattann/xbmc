11cb39cd4eb619659504ceb7c2e5cc5620e4e19a
Lukas Rusak <lorusak@gmail.com>
2020-03-15 11:05:58 -0700

    fixup! gtest: remove lib/gtest and add as library

--- cmake/modules/FindGtest.cmake
+++ cmake/modules/FindGtest.cmake
@@ -52,7 +52,8 @@ if(ENABLE_INTERNAL_GTEST)
 
   externalproject_add(gtest
                       URL ${GTEST_URL}
-                      DOWNLOAD_NAME gtest-${GTEST_VER}.tar.gz
+                      URL_HASH SHA256=58a6f4277ca2bc8565222b3bbd58a177609e9c488e8a72649359ba51450db7d8
+                      DOWNLOAD_NAME gtest-${GTEST_VERSION}.tar.gz
                       DOWNLOAD_DIR ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/download
                       PREFIX ${CORE_BUILD_DIR}/gtest
                       INSTALL_DIR ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}
